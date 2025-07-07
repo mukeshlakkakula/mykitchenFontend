70893607bc725efa4fb38b18aeb5b0dcae9110eb
Mukesh <139950191+mukeshlakkakula@users.noreply.github.com>
2025-07-07 08:49:42 +0530

    Update url for cors

--- cloudkitchen/backend/index.js
+++ cloudkitchen/backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://cloud-kitchenfullone-frontend.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
